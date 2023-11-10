9fc09f6352ec66cab017e98a66119dd00aca4b78
miusarname <oscarm.alvarezg@gmail.com>
2023-11-09 19:37:12 -0500

    feat: :sparkles: The front-end "template" is created

---
+++ view/view/src/views/main.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/header";
+import About from "../components/about";
+import Tobuy from "../components/buyElm";
+
+function Main() {
+  return (
+    <>
+      <Header/>
+      <About/>
+      <Tobuy/>
+    </>
+  )
+}
+
+export default Main
